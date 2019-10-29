197bd8fa30179401dfc45ba480913f6a4aa264d1
Ryner Gabriel <drryner@icloud.com>
2019-10-29 06:47:13 +0300

    Add Miso.SPA exporter module

---
+++ src/Miso/SPA.hs
@@ -0,0 +1,11 @@
+module Miso.SPA
+    ( module Miso
+    , module Miso.SPA.Server
+    , module Miso.SPA.Types
+    , module Miso.SPA.Utils
+    ) where
+
+import           Miso
+import           Miso.SPA.Server
+import           Miso.SPA.Types
+import           Miso.SPA.Utils
