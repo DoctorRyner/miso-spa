5fcfddf4b96706d556e3890e42d8cf42904abcff
drryner <drryner@icloud.com>
2020-02-05 05:25:50 +0300

    FIX: Incomplete modules export

--- src/Miso/SPA.hs
+++ src/Miso/SPA.hs
@@ -3,27 +3,34 @@ module Miso.SPA
     , module Miso.SPA.Types
     , module Miso.SPA.Utils
 
+    , module Miso.Dev
+    , module Miso.Effect
     , module Miso.Event
-    , module Miso.Router
-    , module Miso.Util
-    , module Miso.WebSocket
-
     , module Miso.Html.Event
     , module Miso.Html.Property
-
-    , module Network.URI
+    , module Miso.Mathml
+    , module Miso.Router
+    , module Miso.String
+    , module Miso.Subscription
+    , module Miso.Types
+    , module Miso.Util
+    , module Miso.WebSocket
     ) where
 
 import           Miso.SPA.Server
 import           Miso.SPA.Types
 import           Miso.SPA.Utils
 
+import           Miso.Dev
+import           Miso.Effect
 import           Miso.Event
+import           Miso.Html
+import           Miso.Html.Event
+import           Miso.Html.Property
+import           Miso.Mathml
 import           Miso.Router
+import           Miso.String
+import           Miso.Subscription
+import           Miso.Types
 import           Miso.Util
 import           Miso.WebSocket
-
-import           Miso.Html.Event
-import           Miso.Html.Property hiding (form_)
-
-import           Network.URI
